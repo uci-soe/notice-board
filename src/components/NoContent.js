16ab37c7e21abc986ab24f265c4c13b75b5ff2e6
Rhett Lowe <rng2ml@gmail.com>
2019-09-16 12:53:24 -0700

    adding no-content components for when there are not students, tikets, or events

---
+++ src/components/NoContent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './NoContent.scss';
+
+export function NoTickets () {
+  return (
+    <div></div>
+  );
+}
+
+export function NoEvents () {
+  return (
+    <div></div>
+  );
+}
+
+export function NoStudents () {
+  return (
+    <div></div>
+  );
+}
